d11ef58b09e5a826fcfe3fe87bc57307ce921c11
Alan Perez <alanp9six@gmail.com>
2019-05-30 15:39:00 -0500

    users table completed, haven't migrated yet

--- database/migrations/20190530140356_Users.js
+++ database/migrations/20190530140356_Users.js
@@ -1,8 +1,21 @@
 
 exports.up = function(knex, Promise) {
-  
+  return (
+      knex.schema
+      
+      // USERS TABLE
+      .createTable("Users", users => {
+          users.increments();
+          users.string("CompanyName",128).notNullable();
+          users.string("Country",128).notNullable();
+          users.string("Email",128).notNullable();
+          users.string("PhoneNum",128).notNullable();
+          users.string("Password",16).notNullable();
+      })
+  )
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTableIfExists("Users");
 };
