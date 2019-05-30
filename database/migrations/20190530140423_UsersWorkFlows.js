aa9cb0254bdf0aa0297bd4dec438d64b893a84b8
Alan Perez <alanp9six@gmail.com>
2019-05-30 15:43:43 -0500

    Users Work Flows bridge table completed

--- database/migrations/20190530140423_UsersWorkFlows.js
+++ database/migrations/20190530140423_UsersWorkFlows.js
@@ -1,8 +1,28 @@
 
 exports.up = function(knex, Promise) {
-  
+  return (
+      knex.schema
+        .createTable("UsersWorkFlows", tbl => {
+            tbl.integer('user_id')
+            .unsigned()
+            .notNullable()
+            .references("id")
+            .inTable("Users")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE");
+
+            tbl.integer('workflow_id')
+            .unsigned()
+            .notNullable()
+            .references("id")
+            .inTable("WorkFlows")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE");
+        })
+  )
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTableIfExists('UsersWorkFlows')
 };
