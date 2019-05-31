4b445b406b9ffb2d91d9c8c12bc4eb70e65893eb
Alan Perez <alanp9six@gmail.com>
2019-05-31 11:53:58 -0500

    Front End is able to Pull user data and specefic id

--- models/User-Model.js
+++ models/User-Model.js
@@ -1,9 +1,25 @@
 const db = require("../database/dbConfig");
 
 module.exports = {
-  find
+  find,
+  getBy,
+  getById,
+  add
 };
 
 function find() {
   return db("Users");
+}
+
+function getBy(filter) {
+  return db('Users').where(filter);
+}
+function getById(id) {
+  return db('Users').where({ id }).first();
+}
+
+async function add(user) {
+    const [id] = await db('Users').insert(user);
+
+    return getById(id);
 }
