9e6f3094ce867d27a3e9f8a361e973b44068d749
jurusteve <jstevemu17@gmail.com>
2019-06-01 23:00:52 -0400

    moved all routes to the server file

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 require("dotenv").config();
 const server = require("./api/server");
-const passport = require("./config/passport-setup");
 
 const port = process.env.PORT || 5000;
-
-server.get("/", (req, res) => {
-  res.send("WE ARE LIVE");
-});
 
 server.listen(port, () => {
   console.log(`Server is live at ${port}`);
