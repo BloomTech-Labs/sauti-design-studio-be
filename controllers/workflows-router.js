96e80713f3b9a02165eb412afd2e64dc3bae287d
Alan Perez <alanp9six@gmail.com>
2019-06-05 14:01:11 -0500

    The workflow router is setup.. still working on it... need to seed the workflows table with data to test it

--- controllers/workflows-router.js
+++ controllers/workflows-router.js
@@ -9,6 +9,7 @@ const Workflows = require('../models/workflow-models')
 
 const restricted = require('../controllers/authCheck')
 
+// GETS ALL THE WORKFLOWS
 router.get('/', async(req,res) => {
     try {
         const workflows = await Workflows.getById(req.params.id)
@@ -19,4 +20,41 @@ router.get('/', async(req,res) => {
 
 // GET SPECEFIC ID OF WORKFLOW
 
-router.get('/:id', async)
+router.get('/:id', async (req,res) => {
+    try {
+        const workflows = await Workflows.getById(req.params.id);
+            if (workflows) {
+                res.status(200).json(workflows)
+            } else {
+                res.status(404).json({message : "workflow with taht ID does not exist."})
+            }
+    } catch (error) {
+        res.status(500).json({ message: " Error retrieving that workflow"})    }
+})
+
+// POSTS THE WORKFLOW..
+router.post("/", async (req,res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json({message: "unable to post workflow"})
+    }
+})
+
+// UPDATES THE WORKFLOW
+router.put("/:id", async (req,res) => {
+try {
+    
+} catch (error) {
+    res.status(500).json({ message: "Unable to update this workflow at this time.. please try again later"})
+}
+})
+
+// DELETE WORKFLOW
+router.delete("/:id", async(req,res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json({ message: "Unable to delete this workflow."})
+    }
+})
