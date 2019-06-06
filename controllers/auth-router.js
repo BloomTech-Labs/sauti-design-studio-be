d62d4813c49908ca49d97b3909f1ba8c85cc9ba5
Tristan Grovender <therealtristangrovender@gmail.com>
2019-06-06 18:58:15 -0500

    added facebook passport dependency, made new routers for facebook login, implemented login within passport-setup.

--- controllers/auth-router.js
+++ controllers/auth-router.js
@@ -15,4 +15,18 @@ router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
   res.status(200).redirect("/users");
 });
 
+// Login with facebook
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+router.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+router.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: 'https://sautistudio.netlify.com',
+                                      failureRedirect: '/login' }));
+
 module.exports = router;
