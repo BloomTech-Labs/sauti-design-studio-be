db3e03474e296ce159aba25ebce49fcf5d429306
Tristan Grovender <therealtristangrovender@gmail.com>
2019-06-12 11:41:52 -0500

    bug fixes/cleanup

--- controllers/auth-router.js
+++ controllers/auth-router.js
@@ -29,7 +29,7 @@ router.get(
   "/facebook/redirect",
   passport.authenticate("facebook"),
   (req, res) => {
-    res.status(200).redirect(`https://sautistudio.netlify.com/profile`);
+    res.status(200).redirect(`${process.env.FRONTEND_URL}/profile`);
   }
 );
 
