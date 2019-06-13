5fc6632fe7b88838777ec937619545bc606e76c4
jurusteve <jstevemu17@gmail.com>
2019-06-13 15:55:56 -0400

    :sparkles: integrate ussd route
    Adds the ussd-menu-builder npm package to build states for our flows

--- api/server.js
+++ api/server.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const passport = require('passport');
+const express = require("express");
+const passport = require("passport");
 
 const server = express();
-const cookieSession = require('cookie-session');
-const passportSetup = require('../config/passport-setup');
-const serverConfig = require('./serverConfig');
+const cookieSession = require("cookie-session");
+const passportSetup = require("../config/passport-setup");
+const serverConfig = require("./serverConfig");
 // ROUTES
-const UsersRouter = require('../controllers/users-router');
-const WorkflowsRouter = require('../controllers/workflows-router');
-const UsersWorkflows = require('../controllers/userworkflows-router');
-const ClientsRouter = require('../controllers/clients-router');
-const AnswersRouter = require('../controllers/answers-router');
-const QuestionsRouter = require('../controllers/questions-router');
+const UsersRouter = require("../controllers/users-router");
+const WorkflowsRouter = require("../controllers/workflows-router");
+const UsersWorkflows = require("../controllers/userworkflows-router");
+const ClientsRouter = require("../controllers/clients-router");
+const AnswersRouter = require("../controllers/answers-router");
+const QuestionsRouter = require("../controllers/questions-router");
+const UssdRouter = require("../controllers/ussd-router");
 // Authentication
-const AuthRouter = require('../controllers/auth-router');
-const authCheck = require('../controllers/authCheck');
-const ProfileRouter = require('../controllers/profile-router');
+const AuthRouter = require("../controllers/auth-router");
+const authCheck = require("../controllers/authCheck");
+const ProfileRouter = require("../controllers/profile-router");
 
 // middleware
 serverConfig(server);
@@ -24,12 +25,12 @@ console.log(process.env.NODE_ENV);
 
 server.use(
   cookieSession({
-    name: 'cookie',
+    name: "cookie",
     maxAge: 24 * 60 * 60 * 1000,
     keys: [process.env.SESSION_COOKIE],
     secure: false,
     // httpOnly: true,
-    signed: true,
+    signed: true
   })
 );
 // initialize passport
@@ -37,26 +38,27 @@ server.use(passport.initialize());
 server.use(passport.session());
 
 //  Register and login routes
-server.use('/auth', AuthRouter);
+server.use("/auth", AuthRouter);
 
 // endpoints
-server.use('/profile', authCheck, ProfileRouter);
-server.use('/users', authCheck, UsersRouter);
-server.use('/users-workflows', UsersWorkflows);
-server.use('/workflows', WorkflowsRouter);
-server.use('/clients', ClientsRouter);
-server.use('/answers', AnswersRouter);
-server.use('/questions', QuestionsRouter);
-server.get('/', (req, res) => {
+server.use("/profile", authCheck, ProfileRouter);
+server.use("/users", authCheck, UsersRouter);
+server.use("/users-workflows", UsersWorkflows);
+server.use("/workflows", WorkflowsRouter);
+server.use("/clients", ClientsRouter);
+server.use("/answers", AnswersRouter);
+server.use("/questions", QuestionsRouter);
+server.use("/ussd", UssdRouter);
+server.get("/", (req, res) => {
   res.send(`We're live! Please Login.`);
 });
 
-server.get('/home', (req, res) => {
-  res.status(200).json({ message: 'Success' });
+server.get("/home", (req, res) => {
+  res.status(200).json({ message: "Success" });
 });
 
 // Logout route
-server.get('/logout', (req, res) => {
+server.get("/logout", (req, res) => {
   req.logOut();
   res.status(400).redirect(`${process.env.FRONTEND_URL}`);
 });
