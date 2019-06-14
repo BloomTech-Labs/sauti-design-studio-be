b7d163274d9f97da3c92ced4c7613f06343ae556
jurusteve <jstevemu17@gmail.com>
2019-06-14 14:37:42 -0400

    :art: changed the questions table to remove the options field
    also created a sessions table

--- database/migrations/20190606183050_Questions.js
+++ database/migrations/20190606183050_Questions.js
@@ -1,7 +1,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('questions', tbl => {
     tbl.increments();
-    tbl.string('options', 60).notNullable();
     tbl.string('question_text', 120).notNullable();
     tbl
       .integer('option_number')
