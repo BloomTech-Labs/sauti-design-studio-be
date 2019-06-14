b194d3a217f867a27e676992029c54d82c23f117
jurusteve <jstevemu17@gmail.com>
2019-06-14 15:01:21 -0400

    :sparkles: added ussd-model
    added ussd-model to record the sessions

--- controllers/ussd-router.js
+++ controllers/ussd-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const UssdMenu = require('ussd-menu-builder');
+const UssdModel = require('../models/ussd-model');
 
 // Function to create a new menu. Recommended to create a new menu for each request
 const createMenu = () => {
@@ -11,7 +12,7 @@ const screen = {
   questionText: 'Welcome! What would you like to look up?',
   options: [
     { number: 1, text: 'Show Balance' },
-    { number: 2, text: 'Buy Airtime' },
+    { number: 2, text: 'Buy Airtime' }
   ],
 };
 
@@ -25,11 +26,31 @@ class BuildScreen {
     console.log(this.question, this.options);
   }
 }
+const args = {
+  sessionId: req.body.sessionId,
+};
+
+function getSessionInfo(body) {
+  const session = {
+    sessionId: body.sessionId,
+    phoneNumber: body.phoneNumber,
+    serviceCode: body.serviceCode,
+    text: body.text,
+  };
+  return session;
+}
 // DYNAMIC ROUTE HANDLER
 router.post('/', (req, res) => {
   // create a new menu for each request
   const menu = createMenu();
-
+  const session = getSessionInfo(req.body);
+  const ussdSess = UssdModel.addSession(session.sessionId)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(e => {
+      res.status(500).json(e);
+    });
   // construct questions and options object for a given flow
   const newScreen = new BuildScreen(screen);
 
@@ -60,6 +81,12 @@ router.post('/', (req, res) => {
     },
     next: nextState,
   });
+  menu.state('Home', {
+    run: () => {
+      menu.con(currentQuestion);
+    },
+    next: nextState,
+  });
   menu.state('Show Balance', {
     run: () => {
       const balance = '234,434,344';
@@ -69,10 +96,11 @@ router.post('/', (req, res) => {
 
   menu.state('Buy Airtime', {
     run: () => {
-      menu.con('Enter amount:');
+      menu.con('Enter amount: \n1. Go Back');
     },
     next: {
       '*\\d+': 'buyAirtime.amount',
+      '1': 'Home'
     },
   });
   menu.state('buyAirtime.amount', {
