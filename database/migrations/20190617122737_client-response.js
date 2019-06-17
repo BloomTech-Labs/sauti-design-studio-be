b356678b3646d45b8fd79d2bae451f534552d1c4
Alan Perez <alanp9six@gmail.com>
2019-06-17 14:53:33 -0500

    client response table

--- database/migrations/20190617122737_client-response.js
+++ database/migrations/20190617122737_client-response.js
@@ -1,8 +1,19 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('client_response', tbl => {
+      tbl.increments();
+      tbl
+      .integer('response_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('questions','answers')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('client_response');
 };
