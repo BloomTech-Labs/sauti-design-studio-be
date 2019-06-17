45cd2b21a716f006ab6bf5696eef51b2704550a4
Alan Perez <alanp9six@gmail.com>
2019-06-17 12:08:01 -0500

    User Profile Delete Tested with Postman

--- models/profile-model.js
+++ models/profile-model.js
@@ -3,6 +3,7 @@ const db = require('../database/dbConfig');
 module.exports = {
   getById,
   updateUser,
+  deleteUser
 };
 
 function getById(id) {
@@ -13,4 +14,10 @@ function updateUser(id, info) {
   return db('users')
     .where({ id })
     .update(info);
+}
+
+function deleteUser(id) {
+  return db('users')
+    .where('id', id)
+    .del();
 }
