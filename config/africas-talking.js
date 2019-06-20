25a8dd1f2bb98a580171713eb8e49397aaebc5ce
Garrett Weems <gwgraphicdesign@gmail.com>
2019-06-20 15:36:47 -0500

    🎨 .env

--- config/africas-talking.js
+++ config/africas-talking.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 module.exports = {
   AT: {
-    apiKey: '82793ca1a7f48edda028259ad56468368c6c6f0a6af1b4d610fab01913f608f8', // use your sandbox app API key for development in the test environment
+    apiKey: process.env.AT_API_KEY,
     username: 'sandbox',
     format: 'json',
   },
@@ -9,6 +11,6 @@ module.exports = {
     key: '5f2f3ae5e4171e326f3f',
     secret: 'a3c402f0a3e3d243701f',
     cluster: 'us2',
-    // encrypted: true
+    encrypted: true,
   },
 };
