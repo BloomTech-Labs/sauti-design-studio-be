83ed7f77f9ed050350589609b34b353a77b71e1d
Garrett Weems <gwgraphicdesign@gmail.com>
2019-06-24 14:32:33 -0400

    ✨ zoom state

--- controllers/ussd-router.js
+++ controllers/ussd-router.js
@@ -47,13 +47,7 @@ router.post('/', async (req, res) => {
     const session = getSessionInfo(req.body);
     // Filter returns workflow id EX: 1
     const filter = await UssdModel.addSession(session);
-    // const workflows = await UssdModel.getAllUserWorkflows(filter);
-    // console.log('TCL: workflows', workflows);
     const workflow = await UssdModel.getUserWorkflow(filter);
-    console.log('TCL: userWorkflow', workflow.startScreen.question);
-
-    // const currentOption = makeCurrentOption(newScreen);
-    // console.log('TCL: currentOption', currentOption);
 
     menu.startState({
       run: () => {
@@ -64,56 +58,13 @@ router.post('/', async (req, res) => {
       next: workflow.startScreen.next,
     });
 
-    console.log(
-      'TCL: userWorkflow.startScreen.next',
-      workflow.startScreen.next
-    );
-    // menu.state(userWorkflow.title, {
-    //   run: () => {
-    //     menu.con('Enter amount:');
-    //   },
-    //   next: {
-    //     // using regex to match user input to next state
-    //     '*\\d+': 'buyAirtime.amount',
-    //   },
-    // });
-    menu.state(workflow.startScreen.menu, {
-      run: async () => {
-        menu.end(workflow.startScreen.question);
-        // menu.end(screen.id);
-        // menu.end({
-        //   id: screen.id,
-        //   menu: screen.menu,
-        // });
-      },
-      // next: menu.startScreen.next,
-    });
-
     for (const screen of workflow.screens) {
       menu.state(screen.menu, {
         run: async () => {
           menu.con(await UssdModel.getScreenData(screen.id));
-          // menu.end(screen.id);
-          // menu.end({
-          //   id: screen.id,
-          //   menu: screen.menu,
-          // });
-          console.log('TCL: screen', screen);
         },
       });
     }
-
-    console.log('TCL: menu.state', menu.states);
-
-    menu.state('buyAirtime', {
-      run: () => {
-        menu.con('Enter amount:');
-      },
-      next: {
-        // using regex to match user input to next state
-        1: 'melee',
-      },
-    });
 
     // console.log('TCL: menu', menu);
 
--- models/ussd-model.js
+++ models/ussd-model.js
@@ -17,12 +17,6 @@ const addSession = async session => {
   return filter.workflow;
 };
 
-const makeStartQuestion = (text, options) =>
-  `${text}: ${Object.keys(options)
-    .map(obj => `\n${options[obj].id}. ${options[obj].name}`)
-    .toString()
-    .split(`,`)}`;
-
 const makeCurrentQuestion = (text, options) =>
   `${text}: ${Object.keys(options)
     .map(obj => `\n${options[obj].order}.${options[obj].question_text}`)
@@ -40,19 +34,6 @@ const makeNextObj = (name, array) =>
     menu: _.camelCase(obj.question_text),
     id: obj.order,
   }));
-
-const makeStartScreenNext = workflows =>
-  workflows.map(workflow => ({
-    menu: _.camelCase(workflow.name),
-  }));
-
-class Workflows {
-  constructor(workflows) {
-    this.menu = 'wf';
-    this.question = makeStartQuestion('Workflows:', workflows);
-    this.next = makeStartScreenNext(workflows);
-  }
-}
 
 class Workflow {
   constructor(workflow, questions) {
@@ -76,61 +57,14 @@ const getUserWorkflow = async workflow_id => {
         ...question,
         answers: await db('answers').where({ question: question.id }),
       };
-      console.log('TCL: obj', obj);
-
       return obj;
     })
   );
-  console.log('TCL: questionsAndAnswers', questionsAndAnswers);
 
-  const screen = new Workflow(workflow, await questionsAndAnswers);
-
-  return screen;
+  return new Workflow(workflow, await questionsAndAnswers);
 };
 
 const makeResponseBreaks = arr => arr.map(item => `\n${item.answer_text} `);
-
-// Format options to be displayed to clients
-
-const getAllUserWorkflows = async filter => {
-  const workflows = await db('workflows').where({ user_id: filter.user_id });
-  return new Workflows(workflows);
-};
-
-const getAllScreens = async workflow_id => {
-  const questions = await db('questions')
-    .where({ workflow_id })
-    .orderBy('order');
-  const { name } = await db('workflows')
-    .where({ id: workflow_id })
-    .first();
-
-  const getAnswer = async question_id =>
-    db('answers').where({ question: question_id });
-
-  const makeNext = async questions => {
-    const results = await questions.map(async question =>
-      db('answers').where({ question: question.id })
-    );
-    return results;
-  };
-
-  const title = _.camelCase(name);
-
-  const obj = {
-    startScreen: {
-      menu: title,
-      question: makeCurrentQuestion(name, questions),
-      questions,
-      next: {
-        1: 'topTier',
-        2: 'characterList',
-      },
-    },
-    // screens: makeOtherScreens(title, questions),
-  };
-  return obj;
-};
 
 const getScreenData = async id => {
   const answers = await db('answers').where({ question: id });
@@ -139,9 +73,7 @@ const getScreenData = async id => {
 };
 
 module.exports = {
-  getAllUserWorkflows,
   getUserWorkflow,
   getScreenData,
-  getAllScreens,
   addSession,
 };
