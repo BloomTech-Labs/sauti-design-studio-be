518370a57638e190e5927ae32ca654b1a2470b33
amberm <nekosenpai@gmail.com>
2019-07-25 17:29:45 -0600

    test data types being displayed

--- controllers/testrouter.js
+++ controllers/testrouter.js
@@ -23,8 +23,25 @@ router.get('/', async(req, res) =>{
     // res.send('test output home page')
 })
 
+
+
+// Post receives data like the following from AfricaTalks
+
+// {
+// 	"sessionId": "135351351351",
+// 	"phoneNumber": "8675309",
+// 	"networkCode": "Verizon or Something...",
+// 	"serviceCode": "+44454545?",
+// 	"text": "1"
+// }
+
+
 router.post('/', async (req, res) => {
-    res.send('testoutput for a new screen')
+
+    let textnum = req.body;
+    console.log(textnum.text);
+
+    res.send(`testoutput for a new screen with input ${textnum.text}`)
 })
 
 
