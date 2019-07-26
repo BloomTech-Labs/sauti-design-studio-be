4ee52facd092611a4e78da4ad2d9e36cb00bcdd8
amberm <nekosenpai@gmail.com>
2019-07-26 10:34:35 -0600

    working out screen delivery logic

--- controllers/testrouter.js
+++ controllers/testrouter.js
@@ -9,24 +9,24 @@ const homesesh = () => {
     return db('graphTable').where({id:1});
 }
 
-
+let page = "string";
 
 
 router.get('/', async(req, res) =>{
     
     let homeText = await homesesh();
 
-    console.log(homeText[0]["text"])
+    page = "pogo";
+
+    console.log(homeText[0]["text"]);
+    console.log('page ', page);
     
-    res.send(homeText[0]["text"])
+    res.send(homeText[0]["text"]);
 
     // res.send('test output home page')
 })
 
-
-
 // Post receives data like the following from AfricaTalks
-
 // {
 // 	"sessionId": "135351351351",
 // 	"phoneNumber": "8675309",
@@ -35,13 +35,38 @@ router.get('/', async(req, res) =>{
 // 	"text": "1"
 // }
 
+const newpagehold = async(request, current) => {
+    console.log('newpagehold');
+}
+
+const curscreen = async(current, request) => {
+
+    let newscreen = "blank";
+
+    if (page == "string" || "pogo") {
+        newscreen = await homesesh();
+
+        return newscreen;
+    }
+    else {
+        // newscreen = await newpage();
+
+
+    }
+ 
+}
+
 
 router.post('/', async (req, res) => {
 
-    let textnum = req.body;
-    console.log(textnum.text);
+    let textnum = req.body.text;
 
-    res.send(`testoutput for a new screen with input ${textnum.text}`)
+    let screen = curscreen(page, textnum);
+
+    console.log(textnum.text);
+    console.log('page ', page);
+
+    res.send(`testoutput for a new screen with input ${textnum}`)
 })
 
 
