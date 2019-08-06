b3bc8377529798a840a8254fb3a701ea4ad29003
amberm <nekosenpai@gmail.com>
2019-08-06 17:09:37 -0600

    worked through options, and now storing arrays of option links in db instead of individually

--- models/graphTable-model.js
+++ models/graphTable-model.js
@@ -10,5 +10,17 @@ function find(filter) {
 }
 
 function insert(rowData) {
-    console.log('holder func for insertion')
+    console.log('TCL: add -> workflow', rowData);
+    return db('graphTable')
+        .insert(rowData)
+        .then(() => db('graphTable').where({ id: id }))
+        .catch(err => console.error(err));
 }
+
+// db('analyses').insert({
+//     userid: data.id,
+//     choice: data.choice,
+//     Label:  data.labelG,
+//     Results: data.resultG,
+//     description: data.description
+// }) 
